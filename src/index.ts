04fefb187edcbd65a6963857197d8f7e3c3d3a5c
Pinnguyen <nguyenthanhpin95@gamil.com>
2024-08-24 16:13:14 +0700

    Connect to mongo atlas using mongogb.driver

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import usersRouter from './routes/users.routes'
+import databaseService from './services/database.services'
 const app = express()
 app.use(express.json())
 app.use('/users', usersRouter)
+
+databaseService.connect()
 
 // listen on port 3000
 app.listen(4000, () => {

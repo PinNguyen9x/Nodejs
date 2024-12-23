8577a50b57b9b08d2d3e1a3b03033de7c1607b6d
Pinnguyen <nguyenthanhpin95@gamil.com>
2024-12-23 10:44:46 +0700

    uppdate vercel config

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,15 @@ import { initFolder } from './utils/file'
 const port = envConfig.port
 initFolder()
 const app = express()
+app.set('trust proxy', 1)
 const corsOptions: CorsOptions = {
   origin: isProduction ? envConfig.clientUrl : '*'
 }
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  max: 100, // Limit each IP to 100 requests per window
+  standardHeaders: true,
   legacyHeaders: false // Disable the `X-RateLimit-*` headers
-  // store: ... , // Use an external store for more precise rate limiting
 })
 app.use(express.json())
 app.use(helmet())
